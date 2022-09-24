5c3c62e90d385b8cbefb14ca27ae943aa7e51919
patseph710 <44211466+patseph710@users.noreply.github.com>
2022-09-24 13:11:43 -0400

    Created TUI Exhibit
    Created exhibit meant to illustrate TUI. Committing before integrating scene builder.....just in case.

---
+++ src/components/TUI.java
@@ -0,0 +1,10 @@
+package components;
+
+public class TUI extends Exhibit{
+
+	public TUI(int index) {
+			super(index, "TUI Exhibit", "Before the advent of the GUI... \n there was... \n the TUI.");
+			// TODO Auto-generated constructor stub
+	}
+
+}
