4efa1c0f1aa259bcdd82ba9c13c301c0fdc87af8
chrisdemonte <46972602+chrisdemonte@users.noreply.github.com>
2022-09-24 03:03:39 -0400

    Key Press Update
    -Added Key Press event to move panes left and right
    -Added textures to the back wall and floor

--- src/app/Runner.java
+++ src/app/Runner.java
@@ -2,7 +2,10 @@ package app;
 
 import components.MuseumPane;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -13,6 +16,28 @@ public class Runner extends Application {
 		// TODO Auto-generated method stub
 		MuseumPane root = new MuseumPane(1280, 720);
 		Scene scene = new Scene(root);
+		
+		
+	    EventHandler<KeyEvent> keyPressHandler = new EventHandler<KeyEvent>() { 
+	        @Override 
+	        public void handle(KeyEvent event) { 
+	           //Playing the animation 
+	        System.out.println("keypressed");
+	           if(event.getCode() == KeyCode.LEFT) {
+	        	   MuseumPane.xLayout += 5;
+	        	   MuseumPane.wall.setLayoutX(MuseumPane.xLayout);
+	        	   MuseumPane.floor.setLayoutX(MuseumPane.xLayout);
+	        	   
+	           }
+	           if(event.getCode() == KeyCode.RIGHT) {
+	        	   MuseumPane.xLayout -= 5;
+	        	   MuseumPane.wall.setLayoutX(MuseumPane.xLayout);
+	        	   MuseumPane.floor.setLayoutX(MuseumPane.xLayout);
+	           }
+	        }           
+	     };
+	     
+	    scene.setOnKeyPressed(keyPressHandler);
 		
 		stage.setScene(scene);
 		stage.show();
--- src/components/MuseumPane.java
+++ src/components/MuseumPane.java
@@ -1,10 +1,32 @@
 package components;
 
+
 import javafx.scene.layout.Pane;
 
 public class MuseumPane extends Pane{
 	
-	public MuseumPane(double width, double length) {
-		this.setMinSize(width, length);
+	public static Pane wall;
+	public static Pane floor;
+	public static double xLayout = 0;
+	
+	public MuseumPane(double width, double height) {
+		this.setMinSize(width, height);
+		
+		wall = new Pane();
+		wall.setMinSize(width, 520);
+		wall.setStyle("-fx-background-image: url(\"assets/pixel_white_brick_wall.jpg\"); -fx-background-size: 395 368;");
+		//wall.setStyle("-fx-background-image: url(\"assets/pixel_white_wall.jpg\"); -fx-background-size: 1080 520;");
+		//wall.setStyle("-fx-background-image: url(\"assets/pixel_white_wall.jpg\"); -fx-background-size: 2160 1040;");
+		this.getChildren().add(wall); 
+		
+		floor = new Pane();
+		floor.setMinSize(width, 200);
+		floor.setLayoutY(520);
+		floor.setStyle("-fx-background-image: url(\"assets/pixel_wood_floor.jpg\");-fx-background-size: 512 288;");
+		this.getChildren().add(floor);
+		
+
 	}
+
+	
 }
